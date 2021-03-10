931dcfebb3971e3adbc5e3268469c4f29c43e8b4
Preetham Thelluri <46249739+preethampython101@users.noreply.github.com>
2021-03-09 22:32:31 -0500

    Update wamessagecounter.py

--- wamessagecounter.py
+++ wamessagecounter.py
@@ -3,7 +3,7 @@ print()
 print("""A warm welcome to the WhatsApp chat message counter. This app takes input as a .txt (text) file consisting of the chat history with a specific contact.\nAs you might know, the new updates in WhatsApp don't let us see the message count and some more features.""")
 f = open(sys.argv[1], "r", encoding='utf-8')
 lines = f.readlines()
-messages = -1
+messages = 0
 print("Your chat history has been processed. Give it some time for initialization...")
 ind_mes = {}
 for line in lines:
